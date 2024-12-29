bc0debabcd3ce685786ce9da9467876048f63a4c
AishaSid <114816803+AishaSid@users.noreply.github.com>
2024-12-29 21:07:09 +0500

    updates

--- popup.js
+++ popup.js
@@ -1,14 +1,29 @@
 const marksForm = document.getElementById("grand-marks");
 marksForm.addEventListener("submit", handleMarksFormSubmit);
+// FOR FLEX MARKS --- by fahad sheikh
+const marksForm = document.getElementById("btn");
+if (marksForm) { marksForm.addEventListener('click', handleMarksFormSubmit);}
+else{console.log("there is error");}
 
 const feedbackForm = document.getElementById("feedback-form");
 feedbackForm.addEventListener("submit", handleFeedbackFormSubmit);
+//TO SHOW THE BUTTONS
+const btn2 = document.getElementById("btn-2");
+if (btn2) { btn2.addEventListener('click', predButton);}
+else{ console.log("there is error >.<");}
 
 const gpaCalculatorForm = document.getElementById("gpa-calculator");
 gpaCalculatorForm.addEventListener("submit", handleCalculatorFormSubmit);
+//GRADES
+document.getElementById('grade1').addEventListener('submit', absPredict);
+document.getElementById('grade2').addEventListener('submit', gradePredict);
 
 const admitCardForm = document.getElementById("admit-card");
 admitCardForm.addEventListener("submit", handleAdmitCardSubmit);
+//TO SHOW THE GPA
+const gpa = document.getElementById("gpa");
+if (gpa) { gpa.addEventListener('click', manageGPA);}
+else{ console.log("there is error >.<");}
 
 async function handleMarksFormSubmit(event) {
   event.preventDefault();
@@ -23,6 +38,10 @@ async function handleMarksFormSubmit(event) {
       }
     } catch {}
   }
+//TO SHOW THE Grades
+const grade = document.getElementById("grades");
+if (grade) { grade.addEventListener('click', manageGrades);}
+else{ console.log("there is error >.<");}
 
   chrome.scripting.executeScript({ target: { tabId: tab.id }, function: marksMainFunction });
 }
@@ -63,6 +82,12 @@ async function handleFeedbackFormSubmit(event) {
     alert("Please select a feedback option first.");
     return;
   }
+  Options.classList.add('hidden');
+  if(g_form !== null)
+    {g_form.classList.add('hidden');}
+  
+  var fahad = document.getElementsByClassName("fahad")[0];
+  fahad.innerText = "Credits to Fahad Sheikh xD";
 
   chrome.scripting.executeScript({ target: { tabId: tab.id }, function: feedbackMainFunction, args: [input.value] });
 }
@@ -218,10 +243,46 @@ async function feedbackMainFunction(input) {
 
   input === "Randomize" ? selectRandomFeedback() : selectSpecificFeedback(input);
 } 
+    var fahad = document.getElementsByClassName("fahad")[0];
+    fahad.innerText = (`Credits to Aisha S 22i-1281`);
+    if(g_form !== null)
+    {g_form.classList.add('hidden');}
+}
 
 async function calculatorMainFunction() {
   if (!window.location.href.includes("Student/Transcript")) {
     alert("Please Open Transcript Page first");
+//GRADES
+function manageGrades()
+{
+
+  let body = document.querySelector('body');
+  body.style.backgroundColor = "#2C3845";
+  body.style.color = "#D6DBE0"
+  let btn = document.querySelectorAll('button');
+  btn.forEach((btns)=>
+     {btns.style.backgroundColor = "#E80457"
+      btns.addEventListener('mouseover', () => btns.style.backgroundColor = "#D90368");
+      btns.addEventListener('mouseout', () => btns.style.backgroundColor = "#E80457");
+      btns.addEventListener('mousedown', () => 
+        {
+        btns.style.backgroundColor = "#C81045";
+        btns.style.transform = "scale(0.95)";
+        });
+      btns.addEventListener('mouseup', () => 
+        {
+        btns.style.backgroundColor = "#E80457";
+        btns.style.transform = "scale(1)";
+        });
+     });
+   
+
+  var form1;
+  if(g_form === null)
+  { 
+    form1 = document.getElementById('Grade1');
+    g_form = form1;
+    form1.classList.remove('hidden'); 
     return;
   }
 
@@ -240,13 +301,65 @@ async function calculatorMainFunction() {
       <option value="1" ${currGrade == 'D' ? 'selected' : ''}>D</option>
       <option value="0" ${currGrade == 'F' ? 'selected' : ''}>F</option>
     </select>`;
+  else
+  {
+    g_form.classList.add('hidden');
+    form1 = document.getElementById('Grade1');
   }
+    g_form = form1;
+    form1.classList.remove('hidden');   
+}
+//GPA
+function manageGPA()
+{
+  let body = document.querySelector('body');
+  body.style.backgroundColor = "#1f0450";
+  body.style.color = "#e6d0fc"
+  let btn = document.querySelectorAll('button');
+  btn.forEach((btns)=>
+     {btns.style.backgroundColor = "#4014a0"
+      btns.style.color = "#f3ddf9";
+      btns.addEventListener('mouseover', () => btns.style.backgroundColor = "#5227b1");
+      btns.addEventListener('mouseout', () => btns.style.backgroundColor = "#4014a0");
+      btns.addEventListener('mousedown', () => 
+        {
+        btns.style.backgroundColor = "#27076b";
+        btns.style.transform = "scale(0.95)";
+        });
+      btns.addEventListener('mouseup', () => 
+        {
+        btns.style.backgroundColor = "#4014a0";
+        btns.style.transform = "scale(1)";
+        });
+     });
+
+     var form1;
+     if(g_form === null)
+     { 
+       form1 = document.getElementById('Grade2');
+       g_form = form1;
+       form1.classList.remove('hidden'); 
+       return;
+     }
+     else
+     {
+       g_form.classList.add('hidden');
+       form1 = document.getElementById('Grade2');
+     }
+       g_form = form1;
+       form1.classList.remove('hidden');  
+}
 
   const getSUcredithours = () => {
     return Array.from(document.getElementsByTagName('td'))
         .filter((td) => td.innerText == 'S' || td.innerText == 'U')
         .reduce((total, curr) => total + parseInt(curr.previousElementSibling.innerText), 0);
   }
+//GRADE1 
+function absPredict(event) 
+{
+  event.preventDefault();
+  var abc = document.getElementsByClassName("result1")[0];
 
   let semesters = document.getElementsByClassName("col-md-6");
   let lastSemester = semesters[semesters.length - 1];
@@ -255,6 +368,14 @@ async function calculatorMainFunction() {
   let cgpa = 0;
   let cgpaelem = spans[2];
   let sgpaelem = spans[3];
+  let avg_abs = parseFloat(document.getElementById('avg_abs').value);   //from user
+  let your_abs = parseFloat(document.getElementById('your_abs').value);  
+  let total_abs = parseFloat(document.getElementById('total_abs').value); //from user
+  let rem_abs = 100 - total_abs;
+  let avg_grade = document.getElementById('avg_grade').value; // B
+  let wish_grade = document.getElementById('wish_grade').value;
+let av_perc = avg_abs / total_abs;
+let ans = (rem_abs * av_perc);  
 
   let crEarned = 0;
 
@@ -265,9 +386,18 @@ async function calculatorMainFunction() {
   }
  
   let rows = lastSemester.querySelectorAll('tbody > tr');
+const gradeOrder = [ "F","D", "D+", "C-", "C", "C+", "B-", "B", "B+", "A-", "A", "A+"];
+
+let a = gradeOrder.indexOf(wish_grade);
+let b = gradeOrder.indexOf(avg_grade);
+let ans2 = (a - b) * 4;
 
   for (let row of rows) {
     row.querySelectorAll('td.text-center')[1].innerHTML = getSelect(row.querySelectorAll('td.text-center')[1].innerText);
+if (isNaN(total_abs) || isNaN(avg_abs) || isNaN(your_abs) || (a === -1)|| (b === -1) || total_abs > 100 || avg_abs >= 100 || your_abs >= 100)
+  {
+    abc.innerText = "Please enter valid entries";
+    return;
   }
 
   const getCorrespondingCreditHours = (selectelem) => parseInt(selectelem.parentElement.previousElementSibling.innerText);
@@ -291,6 +421,10 @@ async function calculatorMainFunction() {
       cgpaelem.innerHTML = `CGPA: ${cgpa.toFixed(2)}`;
       sgpaelem.innerHTML = `SGPA: 0`;
       return;
+  if(max_grade === "F" ||  max_grade === undefined)
+    {
+    min_grade ="F";
+    max_grade ="F";
     }
     let calculatedSGPA = totalGradePoints / totalCreditHours;
     let actualCreditHoursEarned = crEarned - getSUcredithours();
@@ -302,7 +436,24 @@ async function calculatorMainFunction() {
     // set cgpaelem and sgpaelem to bold
     cgpaelem.style.fontWeight = 'bold';
     sgpaelem.style.fontWeight = 'bold';
+  abc.innerText = result1;
+  return;
+}
+else if ( (rem_abs) <= (ans2+avg_abs - your_abs + ans +3) )
+  {
+    avg_abs += ans +3;
+    if (ans2+avg_abs >= 100 || ans2+avg_abs <= 20)
+    {
+      abc.innerText = "Please enter valid entries.";
+      return;
+    }
+  let result1 =  (`Total abs req: ${(ans2+avg_abs).toFixed(2)} -> ${wish_grade}\nPredicted avg abs: ${(avg_abs).toFixed(2)} -> ${avg_grade}\n${(ans2+avg_abs - your_abs).toFixed(2)} out of ${(rem_abs).toFixed(2)} Summer me milte hein xD`);
+  abc.innerText = result1;
+  return;
   }
+else
+{
+  avg_abs += ans +3;
 
   // add event listener to all select elements
   Array.from(document.getElementsByTagName('select')).forEach((select) => {
@@ -310,6 +461,15 @@ async function calculatorMainFunction() {
   });
 
   handleSelectChange();
+  if (ans2+avg_abs >= 100  || ans2+avg_abs <= 20)
+    {
+      abc.innerText = "Please enter valid entries.";
+      return;
+    }
+  let result1 =  (`Total abs req: ${(ans2+avg_abs).toFixed(2)} -> ${wish_grade}\nPredicted avg abs: ${(avg_abs).toFixed(2)} -> ${avg_grade}\nYou Need a total of ${(ans2+avg_abs - your_abs).toFixed(2)} abs out of Remaining total abs: ${(rem_abs).toFixed(2)}\n ${typeof(rem_abs)} <= ${typeof(ans2+avg_abs +3 - your_abs)}`);
+  abc.innerText = result1;
+  return;
+}
 }
 
 async function handleAdmitCardSubmit(event) {
@@ -324,6 +484,11 @@ async function handleAdmitCardSubmit(event) {
       }
     } catch {}
   }
+//GRADE2
+function gradePredict(event) 
+{
+  event.preventDefault();
+  var abc = document.getElementsByClassName("result2")[0];
 
   const input = document.getElementById("admit-card-radio");
   if (!input) {
@@ -333,6 +498,7 @@ async function handleAdmitCardSubmit(event) {
 
   chrome.scripting.executeScript({ target: { tabId: tab.id }, function: admitCardMainFunction, args: [input.value] });
 }
+const gradeOrder = [ "F","D", "D+", "C-", "C", "C+", "B-", "B", "B+", "A-", "A", "A+"];
 
 async function admitCardMainFunction(inputValue) {
   if (!(inputValue === "Sessional-I" || inputValue === "Sessional-II" || inputValue === "Final")) {
@@ -349,4 +515,20 @@ async function admitCardMainFunction(inputValue) {
   a.download = `Admit_Card_${inputValue}.pdf`;
   a.click();
   URL.revokeObjectURL(url);
+}  
+  let a = Math.floor((max_abs - avg_abs) / 4);
+  let b = Math.floor((max_abs - your_abs) / 4);
+  let index1 = gradeOrder.length-1-a
+  let index2 = gradeOrder.length-1-b
+  let g1 = gradeOrder[index1];
+  let g2 = gradeOrder[index2];
+  if(index1 <=0)
+    g1 = "F";
+  if(index2 <=0)
+    g2 = "F";   
+
+let res = `Let's say highest is ${max_abs} -> A+ and A is at ${max_abs-4}\nGrade of average is ${avg_abs} ->${g1} and your grade is ${your_abs} -> ${g2}`;
+
+abc.innerText = res;
 }
+
